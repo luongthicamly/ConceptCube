c5817104a2167ab2025328d6092d179db014eb38
Luong Thi Cam Ly <luong_thi_cam_ly@management-partners.co.jp>
2022-02-17 14:57:46 +0700

    update

--- src/components/Pages/HomePage/SecondPage/SecondPage.tsx
+++ src/components/Pages/HomePage/SecondPage/SecondPage.tsx
@@ -16,7 +16,7 @@ function SecondPage() {
         speed: 1500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 2000,
       };
     return (
